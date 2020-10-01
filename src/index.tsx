8f6abc90de37a7401d8b99000a7738fb51aaad1f
Santhosh Kumar M <mskumar@catchpoint.com>
2020-10-01 23:57:50 +0530

    react typescript boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export default function app() {
+export default function App():JSX.Element {
+    const sum = (a: number, b: number): number => {
+        return a + b;
+    }
     return( 
         <h1>
-            Hello world
+            Hello world {sum(3,3)}
         </h1>
     )
 }
+
+ReactDOM.render(<App />, document.getElementById('app-root'))
